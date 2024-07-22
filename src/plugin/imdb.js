741e26e91b92e7d83fa124d967d6bbea783d81aa
M-axie <136266635+M-axie@users.noreply.github.com>
2024-07-22 14:38:06 +0300

    Update imdb.js

--- src/plugin/imdb.js
+++ src/plugin/imdb.js
@@ -1,44 +1,58 @@
-import Tesseract from 'tesseract.js';
-import { writeFile, unlink } from 'fs/promises';
+import axios from 'axios';
 
-const givetextCommand = async (m, Matrix) => {
-  const prefixMatch = m.body.match(/^[\\/!#.]/);
-  const prefix = prefixMatch ? prefixMatch[0] : '/';
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-  const args = m.body.split(' ').slice(1); 
+const imdb = async (m, gss) => {
+  try {
+    const prefixMatch = m.body.match(/^[\\/!#.]/);
+    const prefix = prefixMatch ? prefixMatch[0] : '/';
+    const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+    const text = m.body.slice(prefix.length + cmd.length).trim();
 
-  const validCommands = ['givetext', 'extract'];
+    const validCommands = ['imdb'];
 
-  if (validCommands.includes(cmd)) {
-    if (!m.quoted || m.quoted.mtype !== 'imageMessage') {
-      return m.reply(`Send/Reply with an image to extract text ${prefix + cmd}`);
+    if (!validCommands.includes(cmd)) return;
+
+    if (!text) return m.reply('Give me a series or movie name');
+
+    let fids = await axios.get(http://www.omdbapi.com/?apikey=742b2d09&t=${encodeURIComponent(text)}&plot=full);
+    let imdbt = "";
+    
+    if (fids.data.Response === "False") {
+      return m.reply('Movie or series not found');
     }
 
-    let lang = 'eng'; 
-    if (args.length > 0) {
-      lang = args[0]; 
-    }
+    imdbt += "⚍⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚍\n";
+    imdbt += " IMDB SEARCH\n";
+    imdbt += "⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎⚎\n";
+    imdbt += Title      : ${fids.data.Title}\n;
+    imdbt += Year       : ${fids.data.Year}\n;
+    imdbt += Rated      : ${fids.data.Rated}\n;
+    imdbt += Released   : ${fids.data.Released}\n;
+    imdbt += Runtime    : ${fids.data.Runtime}\n;
+    imdbt += Genre      : ${fids.data.Genre}\n;
+    imdbt += Director   : ${fids.data.Director}\n;
+    imdbt += Writer     : ${fids.data.Writer}\n;
+    imdbt += Actors     : ${fids.data.Actors}\n;
+    imdbt += Plot       : ${fids.data.Plot}\n;
+    imdbt += Language   : ${fids.data.Language}\n;
+    imdbt += Country    : ${fids.data.Country}\n;
+    imdbt += Awards     : ${fids.data.Awards}\n;
+    imdbt += BoxOffice  : ${fids.data.BoxOffice}\n;
+    imdbt += Production : ${fids.data.Production}\n;
+    imdbt += imdbRating : ${fids.data.imdbRating}\n;
+    imdbt += imdbVotes  : ${fids.data.imdbVotes}\n;
 
-    try {
-      const media = await m.quoted.download(); 
-      if (!media) throw new Error('Failed to download media.');
-
-      const filePath = `./${Date.now()}.png`;
-      await writeFile(filePath, media);
-
-      const { data: { text } } = await Tesseract.recognize(filePath, lang, {
-        logger: m => console.log(m)
-      });
-
-      const responseMessage = `Extracted Text:\n\n${text}`;
-      await Matrix.sendMessage(m.from, { text: responseMessage }, { quoted: m }); 
-
-      await unlink(filePath); 
-    } catch (error) {
-      console.error("Error extracting text from image:", error);
-      await Matrix.sendMessage(m.from, { text: 'Error extracting text from image.' }, { quoted: m }); 
-    }
+    await gss.sendMessage(m.from, {
+      image: {
+        url: fids.data.Poster,
+      },
+      caption: imdbt,
+    }, {
+      quoted: m,
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    m.reply('An error occurred while fetching the data.');
   }
 };
 
-export default givetextCommand;
+export default imdb;
